425cd3cd6b0c0ade2284042dd24d676ca59f6fd5
DjakaTechnology <djakajaya89@gmail.com>
2025-06-02 13:47:50 +0700

    Changed import type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,5 @@
-export { default as Engine } from "Engine";
-export { default as Constraint } from "Physics/Constraint";
-export { default as Point } from "Physics/Point";
-export { default as RigidBody } from "Physics/RigidBody";
-export { default as Line } from "Utilities/Line";
-export { default as Types } from "Types";
-export { default as DebugInfo } from "Types";
+import Engine from "Engine";
+
+declare const Nature2D: Engine;
+
+export = Nature2D;
